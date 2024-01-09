4d8ebf1998bdbee49fb7ab9f2c7cb7534dd4e899
Melbourneee <98503742+Melbourneee@users.noreply.github.com>
2024-01-09 14:43:29 +0800

    Add files via upload

---
+++ LAB ACTIVITY 1/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Dierbersinfo extends React.Component {
+    render() {
+        return (
+             <div>
+                <p>From my family to yours mula sa aming pamilya sayo na daw ako.</p>
+             </div>
+        );
+    }
+}
